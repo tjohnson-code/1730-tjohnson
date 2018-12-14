1901df0bb2491b4f1b7b12d08261c8e50af16135
Tim <im1145ks@go.minnstate.edu>
2018-12-13 18:12:39 -0600

    Completed Exercise 3b.

---
+++ tjohnson3b1/Ex3bCalculations.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tjohnson3b1
+{
+    public class Ex3bCalculations
+    {
+        public static decimal GetDiscountPercent (decimal subtotal)
+        {
+            decimal discountPercent = 0m;
+
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+
+            return discountPercent;
+        }
+        public static void GetDiscountPercent(decimal subtotal, out decimal discountPercent)
+        {
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+        }
+        public static decimal CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months)
+        {
+            decimal futureValue = 0m;
+
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+        public static void CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months, ref decimal futureValue)
+        {            
+            for (int i = 0; i < months; i++)
+
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+        }
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+        public static void CelsiusToFahrenheit(double celsius, out double fahrenheit)
+        {
+            fahrenheit = celsius * 9 / 5 + 32;
+        }
+        public static decimal GrossPay(decimal hours, decimal rate)
+        {            
+            decimal result;
+
+            if (hours <= 40)
+                result = hours * rate;
+            else
+                result = (40 * rate) + (hours - 40) * rate * 1.5m;
+
+            return result;
+        }
+        public static decimal TotalHours(string strNumbers)
+        {
+            decimal total = 0;
+            int startIndex = 0;
+
+            while (startIndex < strNumbers.LastIndexOf(' '))
+            {
+                int EndIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, EndIndex - startIndex);
+                Decimal number = Decimal.Parse(strNumber);
+                total += number;
+                startIndex = EndIndex + 1;
+            }
+
+            return total;
+        }
+        public static decimal GrossPay(string strNumbers, decimal rate)
+        {
+            decimal totalHours = TotalHours(strNumbers);
+            return GrossPay(totalHours, rate);
+        }
+    }
+}
