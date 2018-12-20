5f1afa7c6483c018727bced21dd1563bfc96014c
Tim <im1145ks@go.minnstate.edu>
2018-12-19 23:55:12 -0600

    Completed Exercise 3d1.

---
+++ tjohnson3d1/Ex3dCalculations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tjohnson3d1
+{
+    public class Ex3dCalculations
+    {
+        public static string ArrayToString(int[] values)
+        {
+            // Create StringBuilder object: sbResult
+            // loop
+            //     convert current value to string, append to sbResult
+            //     append ", " to sbResult
+            // convert sbResult to string: result
+            // remove last ", " from result
+            // return result
+            StringBuilder sbResult = new StringBuilder(200);
+            
+            foreach (int number in values)
+            {
+                sbResult.Append(number);
+                sbResult.Append(", ");
+
+            }
+            
+            string result = sbResult.ToString();
+            int lastIndex = result.LastIndexOf(", ");
+            return result.Remove(lastIndex);
+
+        }
+
+        public static int ValueCount(int[] values, int searchValue)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value equals searchValue
+            // return counter
+            int count = 0;
+            foreach(int value in values)
+            {
+                if(value == searchValue)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int RangeCount(int[] values, int searchMin, int searchMax)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value between min and max (inclusive)
+            // return counter
+            int count = 0;
+            foreach (int value in values)
+            {
+                if (value >= searchMin && value <= searchMax)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ tjohnson3d1UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tjohnson3d1UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tjohnson3d1UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3db77bd-6329-48a0-b819-f349675735e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
